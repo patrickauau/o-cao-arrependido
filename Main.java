f5f2a9331a05f01bb97444f9d620a155f64aacf3
patrickauau <99296414+patrickauau@users.noreply.github.com>
2022-02-11 13:06:05 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
-print("O cão arrependido")
-for( i=0; i<44 ; i++){
-print("Volta o cão arependido, com suas orelhas tão fartas, com seu osso ruído e com o rabo entre as patas");
+public class OCaoArrependido{
+
+     public static void main(String []args){
+    System.out.println("o cao arrependido");
+    for( i=0; i<44 ; i++){
+print("Volta o cao arrependido, com suas orelhas tao fartas, com seu osso ruido e com o rabo entre as patas");
 }
 if(i>2){
 break;
