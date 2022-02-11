ac06e938cae63a0822624ee6cc5b17b43cb42342
patrickauau <99296414+patrickauau@users.noreply.github.com>
2022-02-11 17:53:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class OCaoArrependido{
     System.out.println("o cao arrependido");
     for( i=0; i<44 ; i++){
 print("Volta o cao arrependido, com suas orelhas tao fartas, com seu osso ruido e com o rabo entre as patas");
-}
+    }
 if(i>2){
 break;
 }
