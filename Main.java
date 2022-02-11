f9aeb3067950fdea039c76bf6f1decc6c50f24c1
patrickauau <99296414+patrickauau@users.noreply.github.com>
2022-02-11 12:11:24 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+print("O cão arrependido")
+for( i=0; i<44 ; i++){
+print("Volta o cão arependido, com suas orelhas tão fartas, com seu osso ruído e com o rabo entre as patas");
+}
+if(i>2){
+break;
+}
