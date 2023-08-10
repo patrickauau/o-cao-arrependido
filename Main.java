27b220593ab2895f3aa26222296a058fa8692fe7
Victor <89421533+L0rdSaiyan@users.noreply.github.com>
2023-08-10 20:34:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-public class Main{
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("O cão arrependido");
 
-     public static void main(String []args){
-    System.out.println("o cao arrependido");
-    for( int i=0; i<44 ; i++){
-System.out.println("Volta o cao arrependido, com suas orelhas tao fartas, com seu osso ruido e com o rabo entre as patas");
+        String verso = "Volta o cão arrependido, com suas orelhas tão fartas, " +
+                      "com seu osso roído e com o rabo entre as patas";
+
+        for (int i = 0; i < 44; i++) {
+            System.out.println(verso);
+        }
     }
-
-
-}
 }
